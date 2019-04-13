3ebb7f03c9bf8fb68ce5346f22ad3c70992b5f29
Roger Allan Silva Teixeira <roger.allan@yahoo.com.br>
2019-04-13 20:02:34 -0300

    Tshow

---
+++ .history/src/app_20190413193454.js
@@ -0,0 +1,38 @@
+const fs = require('fs');
+const path = require('path');
+
+const express = require('express');
+
+const app = express(); 
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
+
+const accountData = fs.readFileSync(
+    path.join(__dirname, 'json', 'accounts.json'), 'utf8'
+);
+
+const accounts = JSON.parse(accountData);
+
+const usersData = fs.readFileSync(
+    path.join(__dirname, 'json', 'accounts.json'), 'utf8'
+);
+
+const users = JSON.parse(usersData);
+
+app.get('/', (req, res) => res.render('index', { title: 'Index'}));
+
+app.get('/savings', (req, res) => {
+    res.render('account', {account: accounts.savings });
+});
+
+app.get('/checking', (req, res) => {
+    res.render('accont', {account: accounts.checking });
+});
+
+app.get('/savings', (req, res) => {
+    res.render('account', {account: accounts.credit });
+});
+
+app.listen(3000, () => console.log('PS Project running on port 3000'));
