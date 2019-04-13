94d8d636dab36860e85404afa6b20275084c749f
Roger Allan Silva Teixeira <roger.allan@yahoo.com.br>
2019-04-13 20:01:35 -0300

    Completed the module2

--- src/app.js
+++ src/app.js
@@ -9,6 +9,34 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => res.render('index', { title: 'Index'}));
+const accountData = fs.readFileSync(
+    path.join(__dirname, 'json', 'accounts.json'), 'utf8'
+);
+
+const accounts = JSON.parse(accountData);
+
+const usersData = fs.readFileSync(
+    path.join(__dirname, 'json', 'accounts.json'), 'utf8'
+);
+
+const users = JSON.parse(usersData);
+
+app.get('/', (req, res) => res.render('index', { title: 'Account Summary', accounts}));
+
+app.get('/savings', (req, res) => {
+    res.render('account', {account: accounts.savings });
+});
+
+app.get('/checking', (req, res) => {
+    res.render('account', {account: accounts.checking });
+});
+
+app.get('/credit', (req, res) => {
+    res.render('account', {account: accounts.credit });
+});
+
+app.get('/profile', (req, res) => {
+    res.render('profile', {user: user[0] });
+});
 
 app.listen(3000, () => console.log('PS Project running on port 3000'));
